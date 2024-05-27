6345c2e9cc4ccc93e31163e1046b7d9c0ac32b13
llddang <bodyness@naver.com>
2024-05-27 23:47:23 +0900

    feat: color, font, border-radius, media query 기준을 상수로 정의
    #1

---
+++ frontend/src/constants.ts
@@ -0,0 +1,42 @@
+export const MAX_WIDTH = '1200px';
+
+export const RESPONSIVE_WIDTH = {
+  mobile: '480px',
+  tablet: '768px',
+  desktop: '1200px',
+};
+
+export const COLOR = {
+  comment: '#666666',
+  border: '#EEEEEE',
+  malibu_light: '#E5EBFF',
+  malibu: '#8AA4FF',
+  malibu_text: '#5773FF',
+  malibu_dark: '#2F5DFF',
+};
+
+export const FONT_STYLE = {
+  sm: '14px "Noto Sans KR", sans-serif',
+  base: {
+    normal: '400 16px "Noto Sans KR", sans-serif',
+    semibold: '600 16px "Noto Sans KR", sans-serif',
+    bold: '700 16px "Noto Sans KR", sans-serif',
+  },
+  lg: {
+    normal: '400 20px "Noto Sans KR", sans-serif',
+    semibold: '600 20px "Noto Sans KR", sans-serif',
+    bold: '700 20px "Noto Sans KR", sans-serif',
+  },
+  xl: {
+    normal: '400 32px "Noto Sans KR", sans-serif',
+    semibold: '600 32px "Noto Sans KR", sans-serif',
+    bold: '700 32px "Noto Sans KR", sans-serif',
+  },
+};
+
+export const BORDER_RADIUS = {
+  sm: '10px',
+  md: '20px',
+  lg: '30px',
+  full: '100%',
+};
