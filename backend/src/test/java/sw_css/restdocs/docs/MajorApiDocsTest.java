b234a647322640dce8947bf42e2b1dab51e62a65
amaran-th <songsy405@naver.com>
2024-05-29 11:54:02 +0900

    chore: RESTDocs 빌드 configurations 추가
    #4

--- backend/src/test/java/sw_css/restdocs/docs/MajorApiDocsTest.java
+++ backend/src/test/java/sw_css/restdocs/docs/MajorApiDocsTest.java
@@ -17,14 +17,16 @@ import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 import org.springframework.restdocs.request.QueryParametersSnippet;
+import sw_css.major.api.MajorController;
 import sw_css.major.application.dto.response.MajorResponse;
 import sw_css.restdocs.RestDocsTest;
 
-@WebMvcTest
+@WebMvcTest(MajorController.class)
 public class MajorApiDocsTest extends RestDocsTest {
     @Test
     @DisplayName("[성공] 특정 단과대학의 학과 목록을 조회할 수 있다.")
     public void findMajors() throws Exception {
+        // given
         final QueryParametersSnippet queryParameters = queryParameters(
                 parameterWithName("keyword").optional().description("검색 단어(option)")
         );
@@ -39,7 +41,11 @@ public class MajorApiDocsTest extends RestDocsTest {
                 new MajorResponse(1L, "정보컴퓨터공학부", LocalDateTime.parse("2024-01-01T00:00:00")),
                 new MajorResponse(2L, "의생명공학부", LocalDateTime.parse("2024-01-01T00:00:00"))
         );
+
+        // when
         when(majorQueryService.findMajors(1L)).thenReturn(response);
+
+        // then
         mockMvc.perform(get("/college/1/major"))
                 .andExpect(status().isOk())
                 .andDo(document("major-find", queryParameters, responseFields));
