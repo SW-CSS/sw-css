807528bde9d24a478b07edce309a9cb1e0c3777b
amaran-th <songsy405@naver.com>
2024-06-03 13:52:21 +0900

    feat: Milestone 정보 입력
    #8

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,120 +1,318 @@
-insert into college (name) values ('인문대학');
-insert into college (name) values ('사회과학대학');
-insert into college (name) values ('자연과학대학');
-insert into college (name) values ('공과대학');
-insert into college (name) values ('사범대학');
-insert into college (name) values ('경제통상대학');
-insert into college (name) values ('경영대학');
-insert into college (name) values ('약학대학');
-insert into college (name) values ('생활과학대학');
-insert into college (name) values ('예술대학');
-insert into college (name) values ('나노과학기술대학');
-insert into college (name) values ('생명자원과학대학');
-insert into college (name) values ('간호대학');
-insert into college (name) values ('의과대학');
-insert into college (name) values ('정보의생명공학대학');
+insert into college (name)
+values ('인문대학');
+insert into college (name)
+values ('사회과학대학');
+insert into college (name)
+values ('자연과학대학');
+insert into college (name)
+values ('공과대학');
+insert into college (name)
+values ('사범대학');
+insert into college (name)
+values ('경제통상대학');
+insert into college (name)
+values ('경영대학');
+insert into college (name)
+values ('약학대학');
+insert into college (name)
+values ('생활과학대학');
+insert into college (name)
+values ('예술대학');
+insert into college (name)
+values ('나노과학기술대학');
+insert into college (name)
+values ('생명자원과학대학');
+insert into college (name)
+values ('간호대학');
+insert into college (name)
+values ('의과대학');
+insert into college (name)
+values ('정보의생명공학대학');
 
-insert into major (college_id, name) values (1, '국어국문학과');
-insert into major (college_id, name) values (1, '일어일문학과');
-insert into major (college_id, name) values (1, '불어불문학과');
-insert into major (college_id, name) values (1, '노어노문학과');
-insert into major (college_id, name) values (1, '중어중문학과');
-insert into major (college_id, name) values (1, '영어영문학과');
-insert into major (college_id, name) values (1, '독어독문학과');
-insert into major (college_id, name) values (1, '한문학과');
-insert into major (college_id, name) values (1, '언어정보학과');
-insert into major (college_id, name) values (1, '사학과');
-insert into major (college_id, name) values (1, '철학과');
-insert into major (college_id, name) values (1, '고고학과');
-insert into major (college_id, name) values (2, '행정학과');
-insert into major (college_id, name) values (2, '정치외교학과');
-insert into major (college_id, name) values (2, '사회복지학과');
-insert into major (college_id, name) values (2, '사회학과');
-insert into major (college_id, name) values (2, '심리학과');
-insert into major (college_id, name) values (2, '문헌정보학과');
-insert into major (college_id, name) values (2, '미디어커뮤니케이션학과');
-insert into major (college_id, name) values (3, '수학과');
-insert into major (college_id, name) values (3, '통계학과');
-insert into major (college_id, name) values (3, '물리학과');
-insert into major (college_id, name) values (3, '화학과');
-insert into major (college_id, name) values (3, '생명과학과');
-insert into major (college_id, name) values (3, '미생물학과');
-insert into major (college_id, name) values (3, '분자생물학과');
-insert into major (college_id, name) values (3, '지질환경과학과');
-insert into major (college_id, name) values (3, '대기환경과학과');
-insert into major (college_id, name) values (3, '해양학과');
-insert into major (college_id, name) values (4, '기계공학부');
-insert into major (college_id, name) values (4, '고분자공학과');
-insert into major (college_id, name) values (4, '유기소재시스템공학과');
-insert into major (college_id, name) values (4, '화공생명·환경공학부 - 화공생명공학전공');
-insert into major (college_id, name) values (4, '화공생명·환경공학부 - 환경공학전공');
-insert into major (college_id, name) values (4, '전기전자공학부 - 전자공학전공');
-insert into major (college_id, name) values (4, '전기전자공학부 - 전기공학전공');
-insert into major (college_id, name) values (4, '전기전자공학부 - 반도체공학전공');
-insert into major (college_id, name) values (4, '조선해양공학과');
-insert into major (college_id, name) values (4, '재료공학부');
-insert into major (college_id, name) values (4, '산업공학과');
-insert into major (college_id, name) values (4, '항공우주공학과');
-insert into major (college_id, name) values (4, '건축공학과');
-insert into major (college_id, name) values (4, '건축학과');
-insert into major (college_id, name) values (4, '도시공학과');
-insert into major (college_id, name) values (4, '사회기반시스템공학과');
-insert into major (college_id, name) values (5, '국어교육과');
-insert into major (college_id, name) values (5, '영어교육과');
-insert into major (college_id, name) values (5, '독어교육과');
-insert into major (college_id, name) values (5, '불어교육과');
-insert into major (college_id, name) values (5, '교육학과');
-insert into major (college_id, name) values (5, '유아교육과');
-insert into major (college_id, name) values (5, '특수교육과');
-insert into major (college_id, name) values (5, '일반사회교육과');
-insert into major (college_id, name) values (5, '역사교육과');
-insert into major (college_id, name) values (5, '지리교육과');
-insert into major (college_id, name) values (5, '윤리교육과');
-insert into major (college_id, name) values (5, '수학교육과');
-insert into major (college_id, name) values (5, '물리교육과');
-insert into major (college_id, name) values (5, '화학교육과');
-insert into major (college_id, name) values (5, '생물교육과');
-insert into major (college_id, name) values (5, '지구과학교육과');
-insert into major (college_id, name) values (5, '체육교육과');
-insert into major (college_id, name) values (6, '무역학부');
-insert into major (college_id, name) values (6, '경제학부');
-insert into major (college_id, name) values (6, '관광컨벤션학과');
-insert into major (college_id, name) values (6, '국제학부');
-insert into major (college_id, name) values (6, '공공정책학부');
-insert into major (college_id, name) values (7, '경영학과');
-insert into major (college_id, name) values (8, '약학대학 - 약학전공');
-insert into major (college_id, name) values (8, '약학대학 - 제약학전공');
-insert into major (college_id, name) values (9, '아동가족학과');
-insert into major (college_id, name) values (9, '의류학과');
-insert into major (college_id, name) values (9, '식품영양학과');
-insert into major (college_id, name) values (9, '실내환경디자인학과');
-insert into major (college_id, name) values (9, '스포츠과학과');
-insert into major (college_id, name) values (10, '음악학과');
-insert into major (college_id, name) values (10, '한국음악학과');
-insert into major (college_id, name) values (10, '미술학과');
-insert into major (college_id, name) values (10, '조형학과');
-insert into major (college_id, name) values (10, '디자인학과');
-insert into major (college_id, name) values (10, '무용학과');
-insert into major (college_id, name) values (10, '예술문화영상학과');
-insert into major (college_id, name) values (11, '나노메카트로닉스공학과');
-insert into major (college_id, name) values (11, '나노에너지공학과');
-insert into major (college_id, name) values (11, '광메카트로닉스공학과');
-insert into major (college_id, name) values (12, '식물생명과학과');
-insert into major (college_id, name) values (12, '원예생명과학과');
-insert into major (college_id, name) values (12, '동물생명자원과학과');
-insert into major (college_id, name) values (12, '식품공학과');
-insert into major (college_id, name) values (12, '생명환경화학과');
-insert into major (college_id, name) values (12, '바이오소재과학과');
-insert into major (college_id, name) values (12, '바이오산업기계공학과');
-insert into major (college_id, name) values (12, '조경학과');
-insert into major (college_id, name) values (12, '식품자원경제학과');
-insert into major (college_id, name) values (12, 'IT응용공학과');
-insert into major (college_id, name) values (12, '바이오환경에너지학과');
-insert into major (college_id, name) values (13, '간호학과');
-insert into major (college_id, name) values (14, '의예과');
-insert into major (college_id, name) values (14, '의학과');
-insert into major (college_id, name) values (15, '정보컴퓨터공학부');
-insert into major (college_id, name) values (15, '정보컴퓨터공학부 - 컴퓨터공학전공');
-insert into major (college_id, name) values (15, '정보컴퓨터공학부 - 인공지능전공');
-insert into major (college_id, name) values (15, '의생명융합공학부');
+insert into major (college_id, name)
+values (1, '국어국문학과');
+insert into major (college_id, name)
+values (1, '일어일문학과');
+insert into major (college_id, name)
+values (1, '불어불문학과');
+insert into major (college_id, name)
+values (1, '노어노문학과');
+insert into major (college_id, name)
+values (1, '중어중문학과');
+insert into major (college_id, name)
+values (1, '영어영문학과');
+insert into major (college_id, name)
+values (1, '독어독문학과');
+insert into major (college_id, name)
+values (1, '한문학과');
+insert into major (college_id, name)
+values (1, '언어정보학과');
+insert into major (college_id, name)
+values (1, '사학과');
+insert into major (college_id, name)
+values (1, '철학과');
+insert into major (college_id, name)
+values (1, '고고학과');
+insert into major (college_id, name)
+values (2, '행정학과');
+insert into major (college_id, name)
+values (2, '정치외교학과');
+insert into major (college_id, name)
+values (2, '사회복지학과');
+insert into major (college_id, name)
+values (2, '사회학과');
+insert into major (college_id, name)
+values (2, '심리학과');
+insert into major (college_id, name)
+values (2, '문헌정보학과');
+insert into major (college_id, name)
+values (2, '미디어커뮤니케이션학과');
+insert into major (college_id, name)
+values (3, '수학과');
+insert into major (college_id, name)
+values (3, '통계학과');
+insert into major (college_id, name)
+values (3, '물리학과');
+insert into major (college_id, name)
+values (3, '화학과');
+insert into major (college_id, name)
+values (3, '생명과학과');
+insert into major (college_id, name)
+values (3, '미생물학과');
+insert into major (college_id, name)
+values (3, '분자생물학과');
+insert into major (college_id, name)
+values (3, '지질환경과학과');
+insert into major (college_id, name)
+values (3, '대기환경과학과');
+insert into major (college_id, name)
+values (3, '해양학과');
+insert into major (college_id, name)
+values (4, '기계공학부');
+insert into major (college_id, name)
+values (4, '고분자공학과');
+insert into major (college_id, name)
+values (4, '유기소재시스템공학과');
+insert into major (college_id, name)
+values (4, '화공생명·환경공학부 - 화공생명공학전공');
+insert into major (college_id, name)
+values (4, '화공생명·환경공학부 - 환경공학전공');
+insert into major (college_id, name)
+values (4, '전기전자공학부 - 전자공학전공');
+insert into major (college_id, name)
+values (4, '전기전자공학부 - 전기공학전공');
+insert into major (college_id, name)
+values (4, '전기전자공학부 - 반도체공학전공');
+insert into major (college_id, name)
+values (4, '조선해양공학과');
+insert into major (college_id, name)
+values (4, '재료공학부');
+insert into major (college_id, name)
+values (4, '산업공학과');
+insert into major (college_id, name)
+values (4, '항공우주공학과');
+insert into major (college_id, name)
+values (4, '건축공학과');
+insert into major (college_id, name)
+values (4, '건축학과');
+insert into major (college_id, name)
+values (4, '도시공학과');
+insert into major (college_id, name)
+values (4, '사회기반시스템공학과');
+insert into major (college_id, name)
+values (5, '국어교육과');
+insert into major (college_id, name)
+values (5, '영어교육과');
+insert into major (college_id, name)
+values (5, '독어교육과');
+insert into major (college_id, name)
+values (5, '불어교육과');
+insert into major (college_id, name)
+values (5, '교육학과');
+insert into major (college_id, name)
+values (5, '유아교육과');
+insert into major (college_id, name)
+values (5, '특수교육과');
+insert into major (college_id, name)
+values (5, '일반사회교육과');
+insert into major (college_id, name)
+values (5, '역사교육과');
+insert into major (college_id, name)
+values (5, '지리교육과');
+insert into major (college_id, name)
+values (5, '윤리교육과');
+insert into major (college_id, name)
+values (5, '수학교육과');
+insert into major (college_id, name)
+values (5, '물리교육과');
+insert into major (college_id, name)
+values (5, '화학교육과');
+insert into major (college_id, name)
+values (5, '생물교육과');
+insert into major (college_id, name)
+values (5, '지구과학교육과');
+insert into major (college_id, name)
+values (5, '체육교육과');
+insert into major (college_id, name)
+values (6, '무역학부');
+insert into major (college_id, name)
+values (6, '경제학부');
+insert into major (college_id, name)
+values (6, '관광컨벤션학과');
+insert into major (college_id, name)
+values (6, '국제학부');
+insert into major (college_id, name)
+values (6, '공공정책학부');
+insert into major (college_id, name)
+values (7, '경영학과');
+insert into major (college_id, name)
+values (8, '약학대학 - 약학전공');
+insert into major (college_id, name)
+values (8, '약학대학 - 제약학전공');
+insert into major (college_id, name)
+values (9, '아동가족학과');
+insert into major (college_id, name)
+values (9, '의류학과');
+insert into major (college_id, name)
+values (9, '식품영양학과');
+insert into major (college_id, name)
+values (9, '실내환경디자인학과');
+insert into major (college_id, name)
+values (9, '스포츠과학과');
+insert into major (college_id, name)
+values (10, '음악학과');
+insert into major (college_id, name)
+values (10, '한국음악학과');
+insert into major (college_id, name)
+values (10, '미술학과');
+insert into major (college_id, name)
+values (10, '조형학과');
+insert into major (college_id, name)
+values (10, '디자인학과');
+insert into major (college_id, name)
+values (10, '무용학과');
+insert into major (college_id, name)
+values (10, '예술문화영상학과');
+insert into major (college_id, name)
+values (11, '나노메카트로닉스공학과');
+insert into major (college_id, name)
+values (11, '나노에너지공학과');
+insert into major (college_id, name)
+values (11, '광메카트로닉스공학과');
+insert into major (college_id, name)
+values (12, '식물생명과학과');
+insert into major (college_id, name)
+values (12, '원예생명과학과');
+insert into major (college_id, name)
+values (12, '동물생명자원과학과');
+insert into major (college_id, name)
+values (12, '식품공학과');
+insert into major (college_id, name)
+values (12, '생명환경화학과');
+insert into major (college_id, name)
+values (12, '바이오소재과학과');
+insert into major (college_id, name)
+values (12, '바이오산업기계공학과');
+insert into major (college_id, name)
+values (12, '조경학과');
+insert into major (college_id, name)
+values (12, '식품자원경제학과');
+insert into major (college_id, name)
+values (12, 'IT응용공학과');
+insert into major (college_id, name)
+values (12, '바이오환경에너지학과');
+insert into major (college_id, name)
+values (13, '간호학과');
+insert into major (college_id, name)
+values (14, '의예과');
+insert into major (college_id, name)
+values (14, '의학과');
+insert into major (college_id, name)
+values (15, '정보컴퓨터공학부');
+insert into major (college_id, name)
+values (15, '정보컴퓨터공학부 - 컴퓨터공학전공');
+insert into major (college_id, name)
+values (15, '정보컴퓨터공학부 - 인공지능전공');
+insert into major (college_id, name)
+values (15, '의생명융합공학부');
 
+/* 마일스톤 */
+insert into milestone_category (group, name, limit_score)
+values ('ACTIVITY', 'SW관련 창업', 100);
+insert into milestone_category (group, name, limit_score)
+values ('ACTIVITY', 'TOPCIT 응시(수준2 이상)', 60);
+insert into milestone_category (group, name, limit_score)
+values ('ACTIVITY', 'PCC 참가(PCCP Lv.1 이상)', 60);
+insert into milestone_category (group, name, limit_score)
+values ('ACTIVITY', 'SW관련 경진대회 및 공모전 수상', 60);
+insert into milestone_category (group, name, limit_score)
+values ('ACTIVITY', 'SW교육 참여', 100);
+insert into milestone_category (group, name, limit_score)
+values ('ACTIVITY', '오픈소스 SW활동', 50);
+insert into milestone_category (group, name, limit_score)
+values ('ACTIVITY', '현장실습(인턴십)', 80);
+insert into milestone_category (group, name, limit_score)
+values ('ACTIVITY', 'R&D 산학협력프로젝트 참여', 50);
+insert into milestone_category (group, name, limit_score)
+values ('ACTIVITY', 'SW관련 특허(등록/출원) SW등록, SW관련 논문', 40);
+insert into milestone_category (group, name, limit_score)
+values ('GLOBAL', '글로벌 행사 참여(창업 제외), 특허, 논문, 자격증, 인턴십, 교육, 경진대회', 120);
+insert into milestone_category (group, name, limit_score)
+values ('GLOBAL', '공인영어성적(TOEIC 기준 700점 이상 또는 G-TELP Level2 65점 이상)', 20);
+insert into milestone_category (group, name, limit_score)
+values ('GLOBAL', '전공 영어분반 수강(F 제외)', 40);
+insert into milestone_category (group, name, limit_score)
+values ('GLOBAL', 'SW관련 해외 MOOC 수강', 20);
+insert into milestone_category (group, name, limit_score)
+values ('COMMUNITY', 'SW가치확산 - SW관련 공공기관 봉사, 교육원 주관 SW 가치확산 활동', 50);
+insert into milestone_category (group, name, limit_score)
+values ('COMMUNITY', '경진대회 및 공모전 행사 참여', 60);
+insert into milestone_category (group, name, limit_score)
+values ('COMMUNITY', '튜터링/멘토링 활동', 30);
+insert into milestone_category (group, name, limit_score)
+values ('COMMUNITY', 'Edwith 부산대 SW강의 수강', 60);
+
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (1, '창업', 100, 1);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (1, '교과', 20, 2);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (1, '비교과', 20, 2);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (2, '수준3 이상', 60, 0);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (2, '수준2 이상', 50, 0);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (3, 'PCCP Lv.3 이상', 60, 0);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (3, 'PCCP Lv.1 이상', 50, 0);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (4, 'SW관련 경진대회 및 공모전 수상', 20, 3);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (5, 'SW교육 참여', 1, 100);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (6, '컨트리뷰션', 50, 1);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (6, '교과목수강', 10, 2);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (7, '현장실습(인턴십)', 20, 4);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (8, 'R&D 산학협력프로젝트 참여', 10, 5);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (9, 'SW관련 특허(등록/출원) SW등록, SW관련 논문', 20, 2);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (10, '글로벌 행사 참여(창업 제외), 특허, 논문, 자격증, 인턴십, 교육, 경진대회', 30, 4);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (11, '공인영어성적(TOEIC 기준 700점 이상 또는 G-TELP Level2 65점 이상)', 20, 1);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (12, '전공 영어분반 수강(F 제외)', 10, 4);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (13, 'SW관련 해외 MOOC 수강', 10, 2);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (14, 'SW가치확산 - SW관련 공공기관 봉사, 교육원 주관 SW 가치확산 활동', 10, 5);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (15, '경진대회 및 공모전 행사 참여', 15, 4);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (16, '튜터링/멘토링 활동', 10, 3);
+insert into 01milestone_work_detail (category_id, name, score, limit_count)
+values (17, 'Edwith 부산대 SW강의 수강', 15, 4);
