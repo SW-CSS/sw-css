bbc570c76e2afe8d9eaf87ead907b8b0f8755463
llddang <bodyness@naver.com>
2024-06-12 22:07:32 +0900

    feat: 어드민 페이지로 이동하는 권한 설정
    #18

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse, type NextRequest } from 'next/server';
+
+import { AuthSliceState } from './store/auth.slice';
+
+export const middleware = (request: NextRequest) => {
+  const authJsonStr = request.cookies.get('auth')?.value;
+  const auth: AuthSliceState = authJsonStr ? JSON.parse(authJsonStr) : null;
+
+  if ((!auth || !auth.isAuth) && request.nextUrl.pathname.startsWith('/admin')) {
+    return Response.redirect(new URL('/sign-in', request.url));
+  }
+  if ((!auth || !auth.isModerator) && request.nextUrl.pathname.startsWith('/admin')) {
+    return Response.redirect(new URL('/', request.url));
+  }
+  const response = NextResponse.next();
+  return response;
+};
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
