9839728704d31a63f689c72d7c0b7bd70dc87aed
llddang <bodyness@naver.com>
2024-06-12 22:06:50 +0900

    feat: 로그인, 로그아웃, 회원가입 페이지 파일 생성

---
+++ frontend/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { FONT_STYLE } from '@/constants';
+import { useAppDispatch } from '@/hocks/redux';
+import { signIn } from '@/store/auth.slice';
+
+const SignIn = () => {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+
+  const handleSignInClick = () => {
+    // TODO: api 연결
+    dispatch(
+      signIn({
+        token: 'token',
+        username: 'name',
+        uid: '1',
+        isModerator: true,
+      }),
+    );
+    router.back();
+  };
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '200px',
+        font: FONT_STYLE.lg.normal,
+      }}
+    >
+      로그인 페이지
+      <button
+        type="button"
+        onClick={handleSignInClick}
+        style={{
+          font: FONT_STYLE.lg.normal,
+        }}
+      >
+        자동 로그인
+      </button>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ frontend/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,3 @@
+const SignUp = () => <div>회원가입 페이지</div>;
+
+export default SignUp;
