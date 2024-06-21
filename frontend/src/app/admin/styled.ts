91e7b87627002465612eeeef26b964c1a4ef0d7e
llddang <bodyness@naver.com>
2024-06-21 12:37:56 +0900

    fix: eslint.json 파싱 에러 해결
    #18

---
+++ frontend/src/app/admin/styled.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+import { BORDER_RADIUS, COLOR, FONT_STYLE } from '@/constants';
+
+export const AdminButton = styled.button`
+  padding: 4px 8px;
+  border-radius: ${BORDER_RADIUS.sm};
+  border: none;
+  font: ${FONT_STYLE.base.normal};
+  cursor: pointer;
+`;
+
+export const AdminBlueButton = styled(AdminButton)`
+  background-color: ${COLOR.admin_point};
+  color: white;
+`;
+
+export const AdminRedButton = styled(AdminButton)`
+  background-color: ${COLOR.admin_red};
+  color: white;
+`;
+
+export const AdminBlackButton = styled(AdminButton)`
+  background-color: black;
+  color: white;
+`;
+
+export const AdminGrayButton = styled(AdminButton)`
+  background-color: ${COLOR.admin_sub_point_light};
+  border: 1px solid ${COLOR.admin_sub_point};
+  color: black;
+`;
