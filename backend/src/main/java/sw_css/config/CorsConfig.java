d6bed9b6b5869386ca550e853d629e759592de23
amaranth <81465068+amaran-th@users.noreply.github.com>
2024-06-25 01:40:06 +0900

    Feature/#17 에러 핸들링 코드 작성 (#30)
    * feat: 에러 핸들링 코드 작성
    
    #17
    
    * build: CORS 설정
    
    #17
    
    * chore: 에러 메시지 수정
    
    #17
    
    ---------
    
    Co-authored-by: amaran-th <songsy405@naver.com>

---
+++ backend/src/main/java/sw_css/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package sw_css.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${data.client-url}")
+    private String clientUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(clientUrl)
+                .allowedMethods("*")
+                .exposedHeaders(HttpHeaders.LOCATION)
+                .allowedHeaders("*");
+    }
+}
