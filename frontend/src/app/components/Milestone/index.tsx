bffdd9b68ab53445ab29d7efe34de4b5059da85e
llddang <bodyness@naver.com>
2024-06-24 18:00:16 +0900

    feat: 메인페이지에서 마일스톤 호출
    #32

---
+++ frontend/src/app/components/Milestone/index.tsx
@@ -0,0 +1,10 @@
+import { Description, Title } from '../styled';
+
+const Milestone = () => (
+  <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <Title>나의 마일스톤</Title>
+    <Description>나의 마일스톤 내역을 확인할 수 있어요.</Description>
+  </div>
+);
+
+export default Milestone;
