334d2e89a7763f319a9b071174bb4987d6b9ec4c
amaran-th <songsy405@naver.com>
2024-06-24 14:41:09 +0900

    feat: 마일스톤 정보 페이지 구현
    #23

---
+++ frontend/src/app/milestone/styled.ts
@@ -0,0 +1,61 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+import { BORDER_RADIUS, COLOR, FONT_STYLE, RESPONSIVE_WIDTH } from '@/constants';
+
+interface ResponsiveImageProps {
+  maxHeight: string;
+  maxWidth: string;
+}
+
+export const ContentWrapper = styled.div`
+  background-color: ${COLOR.border};
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  background-color: white;
+  padding: 60px 20px 20px 20px;
+  border-radius: ${BORDER_RADIUS.sm};
+`;
+
+export const Title = styled.div`
+  color: ${COLOR.malibu_text};
+  font: ${FONT_STYLE.xl.bold};
+  margin: 20px auto;
+`;
+
+export const Description = styled.div`
+  text-align: center;
+  margin-bottom: 50px;
+`;
+
+export const InformationList = styled.ul`
+  margin-top: 70px;
+`;
+
+export const Information = styled.li`
+  display: flex;
+  margin-bottom: 40px;
+`;
+
+export const InformationTitle = styled.div`
+  color: ${COLOR.malibu_text};
+  font: ${FONT_STYLE.lg.semibold};
+  width: 220px;
+`;
+export const ImageWrapper = styled.div<ResponsiveImageProps>`
+  display: flex;
+  justify-content: center;
+  img {
+    position: relative !important;
+    height: 100%;
+    width: 100%;
+    max-height: ${(props) => props.maxHeight};
+    max-width: ${(props) => props.maxWidth};
+    object-fit: cover;
+  }
+`;
