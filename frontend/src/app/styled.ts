9e1f2b5e32480a48eecfb5213eeedbe0fe376f38
llddang <bodyness@naver.com>
2024-06-24 16:54:57 +0900

    design: 메인 페이지의 layout 작업
    #32

--- frontend/src/app/styled.ts
+++ frontend/src/app/styled.ts
@@ -33,3 +33,33 @@ export const AdminPageWrapper = styled.div`
 export const AdminPageLayout = styled.div`
   min-height: calc(100vh - ${ADMIN_HEADER_HEIGHT} - 50px);
 `;
+
+export const MainPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContentWrapper = styled.div`
+  width: 100%;
+  margin-top: 40px;
+`;
+
+export const FlexWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  @media screen and (max-width: ${RESPONSIVE_WIDTH.desktop}) {
+    flex-direction: column;
+    gap: 0;
+  }
+`;
+
+export const FlexContentWrapper = styled(ContentWrapper)`
+  width: 800px;
+  @media screen and (max-width: ${RESPONSIVE_WIDTH.desktop}) {
+    width: 100%;
+  }
+`;
+
+export const AnnouncementContent = styled(ContentWrapper)`
+  flex-grow: 1;
+`;
