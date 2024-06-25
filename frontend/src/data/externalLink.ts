ac131d5fa2849404775c28d1c473d84b12fbc90d
llddang <bodyness@naver.com>
2024-06-26 01:23:56 +0900

    chore: 외부 링크 관련 데이터 정의
    #37

---
+++ frontend/src/data/externalLink.ts
@@ -0,0 +1,16 @@
+export const externalLinkInfos: { title: string; url: string; img: string }[] = [
+  {
+    title: '부산대학교<br/>학생지원시스템',
+    url: 'http://onestop.pusan.ac.kr/',
+    img: '/pngs/external_link_1_onestop.png',
+  },
+  { title: '스마트교육플랫폼<br/>PLATO', url: 'http://plato.pusan.ac.kr/', img: '/pngs/external_link_2_plato.png' },
+  { title: '온라인강좌<br/>(edwith)', url: 'http://edwith.org/ptnr/pnuswedu', img: '/pngs/external_link_3_edwith.png' },
+  { title: 'TOPCIT', url: 'http://topcit.or.kr/', img: '/pngs/external_link_4_topcit.png' },
+  {
+    title: '학생 맞춤형<br/>학업 성취 분석 서비스',
+    url: 'http://bd.pusan.ac.kr/',
+    img: '/pngs/external_link_5_pnu_achieve.png',
+  },
+  { title: 'PNU<br/>Online Judge', url: 'http://oj.pusan.ac.kr/', img: '/pngs/external_link_6_pnu_oj.png' },
+];
