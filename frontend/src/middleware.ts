56c8e09cc3ab2527f19abf119ea6f67fd1b40e0e
llddang <bodyness@naver.com>
2024-06-25 14:55:06 +0900

    chore: 검증 관련 로직 할일 추가
    #35

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -6,6 +6,8 @@ export const middleware = (request: NextRequest) => {
   const authJsonStr = request.cookies.get('auth')?.value;
   const auth: AuthSliceState = authJsonStr ? JSON.parse(authJsonStr) : null;
 
+  // TODO: cookie는 사용자가 변조 가능함. 그래서 확인만하는 것이 아닌 검증과증이 필요.
+  // token을 API로 보내 진짜 관리자인지 확인하는 과정 추가하기.
   if ((!auth || !auth.isAuth) && request.nextUrl.pathname.startsWith('/admin')) {
     return Response.redirect(new URL('/sign-in', request.url));
   }
