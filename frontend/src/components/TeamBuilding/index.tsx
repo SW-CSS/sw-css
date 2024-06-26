720cac2d5eefe1fd6aea707ec5fec6897ffb1f74
llddang <bodyness@naver.com>
2024-06-26 16:11:28 +0900

    feat: 팀빌딩 컴포넌트 구현
    #38

---
+++ frontend/src/components/TeamBuilding/index.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import { CgEye } from 'react-icons/cg';
+
+import { COLOR, TEAM_STATUS } from '@/constants';
+import { TeamBuildingInfo } from '@/types';
+
+import * as S from './styled';
+
+const TeamBuilding = ({ id, category, status, title, developer, designer, artist, other, views }: TeamBuildingInfo) => {
+  const recruitment = [
+    { img: '/pngs/team_type_img_1.png', text: '개발', count: developer },
+    { img: '/pngs/team_type_img_2.png', text: '디자인', count: artist },
+    { img: '/pngs/team_type_img_3.png', text: '기획', count: designer },
+    { img: '/pngs/team_type_img_4.png', text: '기타', count: other },
+  ];
+  return (
+    <S.TeamBuildingWrapper href={`/team-building/${id}`}>
+      <S.TeamHeaderWrapper>
+        <S.CategoryText>{category}</S.CategoryText>
+        <S.StatusText color={TEAM_STATUS[status].color}>{TEAM_STATUS[status].text}</S.StatusText>
+      </S.TeamHeaderWrapper>
+      <S.TeamBodyWrapper>
+        <S.TeamTitle>{title}</S.TeamTitle>
+        <S.RecruitmentWrapper>
+          {recruitment.map((item) => {
+            if (item.count !== 0) {
+              return (
+                <S.RecruitmentItem key={`${id}-${item.text}`}>
+                  <Image src={item.img} alt={item.text} width="30" height="30" priority={false} />
+                  <S.RecruitmentItemText>
+                    {item.text}
+                    <br />
+                    <span style={{ color: COLOR.black_text }}>{item.count}명</span>
+                  </S.RecruitmentItemText>
+                </S.RecruitmentItem>
+              );
+            }
+            return null;
+          })}
+        </S.RecruitmentWrapper>
+        <S.ViewDiv>
+          <CgEye color={COLOR.comment} />
+          {views}
+        </S.ViewDiv>
+      </S.TeamBodyWrapper>
+    </S.TeamBuildingWrapper>
+  );
+};
+
+export default TeamBuilding;
