ad96cc713f5d257dee66bd93efeb9e2bdfe0acc2
amaranth <81465068+amaran-th@users.noreply.github.com>
2024-06-26 13:01:05 +0900

    Feature/#23 마일스톤 정보조회 페이지 (#31)
    * feat: 마일스톤 정보 페이지 구현
    
    #23
    
    * feat: 마일스톤 페이지 반응형 처리
    
    #23
    
    * chore: 불필요한 import문 제거
    
    #23
    
    * feat: 2번째 그림의 배경 이미지 적용
    
    #23
    
    * chore: 마일스톤 페이지의 폴더 경로 수정
    
    #23
    
    * design: 변경된 색상 상수명 적용
    
    #23
    
    ---------
    
    Co-authored-by: amaran-th <songsy405@naver.com>

--- frontend/src/app/(withSidebar)/milestone/page.tsx
+++ frontend/src/app/(withSidebar)/milestone/page.tsx
@@ -1,5 +1,47 @@
-import { ContentWrapper } from './styled';
+import Image from 'next/image';
 
-const Page = () => <ContentWrapper>aaa</ContentWrapper>;
+import * as S from './styled';
+
+const Page = () => (
+  <S.ContentWrapper>
+    <S.Content>
+      <S.ImageWrapper maxWidth="230px" maxHeight="66px">
+        <Image src="/images/milestone_img01.png" priority={false} alt="" fill />
+      </S.ImageWrapper>
+      <S.Description>
+        <S.Title>마일스톤이란</S.Title>
+        마일스톤은 전공자들의 SW역량을 종합적으로 평가하기 위한 역량평가지수입니다.
+        <br />
+        학생들은 교내외 여러 활동들을 통하여 실전적 SW역량, 글로벌 역량, 커뮤니케이션 역량을 균형있게 함양하고
+        SW중심대학사업단에서는 학생들의 적립된 마일스톤 점수에 따라 매년 장학생을 선발하고 있습니다.
+      </S.Description>
+      <S.ImageWrapper maxWidth="429px" maxHeight="208px" backgroundImage="/images/milestone_img02_bg.png">
+        <Image src="/images/milestone_img02.png" priority={false} alt="" fill />
+      </S.ImageWrapper>
+      <S.InformationList>
+        <S.Information>
+          <S.InformationTitle>마일스톤 획득 방법</S.InformationTitle>
+          각 영역별 황동 수행 시, 책정 기준에 따라 마일스톤을 획득할 수 있습니다.
+          <br />
+          상세 내용은 아래 표를 참고해주세요.
+        </S.Information>
+        <S.Information>
+          <S.InformationTitle>마일스톤 평가기간</S.InformationTitle>
+          전년도 9월부터 당해년도 8월까지의 실적
+          <br />※ SW 창업, 오픈소스 SW 컨트리뷰션의 경우 당해년도 1월부터 9월까지의 실적만을 반영함.
+        </S.Information>
+        <S.Information>
+          <S.InformationTitle>마일스톤 확인 방법</S.InformationTitle>
+          SW역량지원시스템에서는 나의 마일스톤 현황을 한 눈에 볼 수 있도록 제공하고 있습니다.
+          <br />
+          로그인 후, 메인 페이지와 마이페이지에서 확인하실 수 있습니다.
+        </S.Information>
+      </S.InformationList>
+      <S.ImageWrapper maxWidth="890px" maxHeight="1028px">
+        <Image src="/images/milestone_img03.png" priority={false} alt="" fill />
+      </S.ImageWrapper>
+    </S.Content>
+  </S.ContentWrapper>
+);
 
 export default Page;
--- frontend/src/app/(withSidebar)/milestone/styled.ts
+++ frontend/src/app/(withSidebar)/milestone/styled.ts
@@ -2,9 +2,67 @@
 
 import styled from 'styled-components';
 
-import { BORDER_RADIUS, COLOR } from '@/constants';
+import { BORDER_RADIUS, COLOR, FONT_STYLE, RESPONSIVE_WIDTH } from '@/constants';
+
+interface ResponsiveImageProps {
+  maxHeight: string;
+  maxWidth: string;
+  backgroundImage?: string;
+}
 
 export const ContentWrapper = styled.div`
-  background-color: ${COLOR.white};
+  background-color: ${COLOR.border};
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  background-color: white;
+  padding: 60px 20px 20px 20px;
   border-radius: ${BORDER_RADIUS.sm};
+`;
+
+export const Title = styled.div`
+  color: ${COLOR.primary.main};
+  font: ${FONT_STYLE.xl.bold};
+  margin: 20px auto;
+`;
+
+export const Description = styled.div`
+  text-align: center;
+  margin-bottom: 50px;
+`;
+
+export const InformationList = styled.ul`
+  margin-top: 70px;
+`;
+
+export const Information = styled.li`
+  display: flex;
+  margin-bottom: 40px;
+  @media screen and (max-width: ${RESPONSIVE_WIDTH.mobile}) {
+    display: block;
+  }
+`;
+
+export const InformationTitle = styled.div`
+  color: ${COLOR.primary.main};
+  font: ${FONT_STYLE.lg.semibold};
+  width: 220px;
+  @media screen and (max-width: ${RESPONSIVE_WIDTH.mobile}) {
+    margin-bottom: 10px;
+  }
+`;
+export const ImageWrapper = styled.div<ResponsiveImageProps>`
+  display: flex;
+  justify-content: center;
+  ${({ backgroundImage }) => backgroundImage && `background-image: url(${backgroundImage});`}
+  background-position: center;
+  img {
+    position: relative !important;
+    height: 100%;
+    width: 100%;
+    max-height: ${(props) => props.maxHeight};
+    max-width: ${(props) => props.maxWidth};
+    object-fit: cover;
+  }
 `;
