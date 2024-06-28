885981a70e7c6a5611c7e086999404e2411d92d8
llddang <bodyness@naver.com>
2024-06-28 15:36:24 +0900

    feat: 마일스톤 차트와 테이블 추가
    #56

---
+++ frontend/src/app/components/Milestone/styled.ts
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+export const MilestoneChartWrapper = styled.div`
+  margin: 12px;
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  justify-content: space-around;
+`;
