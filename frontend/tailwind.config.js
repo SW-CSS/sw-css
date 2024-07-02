948799cae37e5fa55e6c9a646582de20fa81abe2
llddang <bodyness@naver.com>
2024-07-02 12:01:06 +0900

    feat: tailwind에 색상 정의
    #71

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,26 @@ module.exports = {
         md: '768px',
         lg: '1200px',
       },
+      colors: {
+        white: '#FFFFFF',
+        black_text: '#333333',
+        comment: '#898C96',
+        border: '#EEEEF0',
+        background: {
+          light: '#F0F1F7',
+          base: '#D2D4DC',
+        },
+        primary: {
+          light: '#E6EBFD',
+          main: '#5773F1',
+          dark: '#1A40EB',
+        },
+        secondary: {
+          light: '#64709B',
+          main: '#26325C',
+          dark: '#050A1C',
+        },
+      },
     },
   },
   plugins: [],
