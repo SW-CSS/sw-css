3c18341a9b6469a4acb68488b607f7f9515c5d77
llddang <bodyness@naver.com>
2024-07-02 17:34:38 +0900

    feat: 첫번째 회원가입 컴포넌트 회원가입 페이지에 추가
    #53

--- frontend/src/app/(auth)/sign-up/page.tsx
+++ frontend/src/app/(auth)/sign-up/page.tsx
@@ -1,3 +1,40 @@
-const Page = () => <div>회원가입 페이지</div>;
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import PageTitle from '@/app/components/PageTitle';
+
+import SignUpFirstPage, { FirstInfo } from './components/SignUpFirstPage';
+
+type UserInformation = FirstInfo;
+
+const Page = () => {
+  const [userInfo, setUserInfo] = useState<UserInformation>({
+    email: '',
+    password: '',
+    name: '',
+    studentId: '',
+    phoneNumber: '',
+  });
+  const [isPhaseOne, setIsPhaseOne] = useState<boolean>(true);
+
+  const handleNextButtonClick = (value: FirstInfo) => {
+    setUserInfo((prev) => ({ ...prev, ...value }));
+    setIsPhaseOne(false);
+  };
+
+  useEffect(() => {
+    console.log(JSON.stringify(userInfo, null, 2));
+  }, [userInfo]);
+
+  return (
+    <div className="w-sign mx-auto pb-10 pt-14 lg:pt-20">
+      <div className="flex flex-col gap-10 p-5 lg:pt-20">
+        <PageTitle title="회원가입" description="PNU SW역량시스템 첫 사용시 회원가입이 필요합니다." urlText="" url="" />
+        {isPhaseOne && <SignUpFirstPage handleSubmitButtonClick={handleNextButtonClick} />}
+      </div>
+    </div>
+  );
+};
 
 export default Page;
