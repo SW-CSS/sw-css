920fd3f1ed440fc7f85d646e463c012507c7c387
llddang <77055208+llddang@users.noreply.github.com>
2024-07-02 12:53:58 +0900

    Feature/#75 페이지 타이틀 컴포넌트 구현 (#76)
    * feat: PageTitle 구현
    
    #75
    
    * feat: 제목과 설명 사이에 gap 설정 + Link 조건 추가
    
    #75
    
    * design: flex 자리 배치 변경
    
    #75

---
+++ frontend/src/app/components/PageTitle/index.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { VscAdd } from 'react-icons/vsc';
+
+export interface PageTitleProps {
+  title: string;
+  description: string;
+  urlText: string;
+  url: string;
+}
+
+const PageTitle = ({ title, description, urlText, url }: PageTitleProps) => (
+  <div className="flex justify-between">
+    <div className="flex flex-col gap-2">
+      <p className="cursor-default text-xl font-semibold">{title}</p>
+      <p className="cursor-default text-sm text-comment">{description}</p>
+    </div>
+    {urlText !== '' && (
+      <Link href={url} className="flex items-center gap-1 text-sm text-comment">
+        <VscAdd />
+        {urlText}
+      </Link>
+    )}
+  </div>
+);
+
+export default PageTitle;
