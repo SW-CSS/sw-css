b0137d0818878eb5f43fefa1ce470b5b4365fc22
llddang <77055208+llddang@users.noreply.github.com>
2024-07-02 12:10:51 +0900

    Feature/#71 tailwind 반응형 사이즈 커스텀 (#72)
    * fix: tailwind css 적용
    
    #71
    
    * feat: 반응형 크기 적용
    
    #71
    
    * feat: tailwind에 색상 정의
    
    #71
    
    * feat: tailwind에 fontSize 설정
    
    #71
    
    * feat: tailwind에 borderRadius 설정
    
    #71
    
    * feat: 기본 폰트 색상 정의
    
    #71

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,53 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '1200px',
+      },
+      colors: {
+        white: '#FFFFFF',
+        black: '#333333',
+        comment: '#898C96',
+        border: '#EEEEF0',
+        background: {
+          light: '#F0F1F7',
+          base: '#D2D4DC',
+        },
+        primary: {
+          light: '#E6EBFD',
+          main: '#5773F1',
+          dark: '#1A40EB',
+        },
+        secondary: {
+          light: '#64709B',
+          main: '#26325C',
+          dark: '#050A1C',
+        },
+      },
+      fontSize: {
+        xs: ['12px', '12px'],
+        sm: ['14px', '14px'],
+        base: ['16px', '16px'],
+        lg: ['20px', '20px'],
+        xl: ['32px', '32px'],
+      },
+      borderRadius: {
+        none: '0',
+        DEFAULT: '4px',
+        sm: '10px',
+        md: '20px',
+        lg: '30px',
+        full: '100%',
+      },
+    },
   },
   plugins: [],
 };
