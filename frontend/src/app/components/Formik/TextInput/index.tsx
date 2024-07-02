b3d6d45b0c9a6b6622e64527579a4186ab6aabb8
llddang <bodyness@naver.com>
2024-07-02 17:33:35 +0900

    feat: TextInput 구현
    #53

---
+++ frontend/src/app/components/Formik/TextInput/index.tsx
@@ -0,0 +1,40 @@
+type BuiltInTextInputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+
+interface CustomTextInputProps {
+  name: string;
+  label: string;
+  isRequired?: boolean;
+  errorText?: string;
+  onKeyDownEnter?(): void;
+  onChangeText?(text: string): void;
+}
+
+export type TextInputProps = BuiltInTextInputProps & CustomTextInputProps;
+
+export const TextInput = ({ isRequired = false, ...props }: TextInputProps) => {
+  const { label, errorText, onKeyDownEnter, onChangeText, ...inputProps } = props;
+  const hasError = errorText !== undefined;
+
+  return (
+    <div className="flex flex-col gap-1">
+      <label htmlFor={inputProps.id || inputProps.name} className="text-sm font-semibold">
+        {label} {isRequired && <span className="text-sm font-semibold text-red-400">*</span>}
+      </label>
+      <input
+        {...inputProps}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            onKeyDownEnter?.();
+          }
+          inputProps.onKeyDown?.(e);
+        }}
+        onChange={(e) => {
+          inputProps.onChange?.(e);
+          onChangeText?.(e.target.value);
+        }}
+        className={`m-0 rounded-sm border-[1px] border-border p-3 text-base ${hasError && 'border-red-400'}`}
+      />
+      {errorText && <span className="pl-1 text-sm text-red-400">{errorText}</span>}
+    </div>
+  );
+};
