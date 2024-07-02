eb89680a5004fa5ce1ab5745f607e2163bde63c7
llddang <bodyness@naver.com>
2024-07-02 12:44:56 +0900

    feat: PageTitle 구현
    #75

---
+++ frontend/src/app/components/PageTitle/index.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import { VscAdd } from 'react-icons/vsc';
+
+export interface PageTitleProps {
+  title: string;
+  description: string;
+  urlText: string;
+  url: string;
+}
+
+const PageTitle = ({ title, description, urlText, url }: PageTitleProps) => (
+  <div className="flex">
+    <div className="flex flex-col">
+      <p className="cursor-default text-xl font-semibold">{title}</p>
+      <p className="cursor-default text-sm text-comment">{description}</p>
+    </div>
+    <Link href={url} className="flex items-center gap-1 text-sm text-comment">
+      <VscAdd />
+      {urlText}
+    </Link>
+  </div>
+);
+
+export default PageTitle;
