7d0f200b17b4be64224345c281aa35bab6242839
llddang <bodyness@naver.com>
2024-07-03 17:39:15 +0900

    design: 회원가입 페이지에 두번째 회원가입 컴포넌트 추가
    #53

--- frontend/src/app/(auth)/sign-up/page.tsx
+++ frontend/src/app/(auth)/sign-up/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import PageTitle from '@/app/components/PageTitle';
 
 import SignUpFirstPage, { FirstInfo } from './components/SignUpFirstPage';
+import SignUpSecondPage, { SecondInfo } from './components/SignUpSecondPage';
 
-type UserInformation = FirstInfo;
+type UserInformation = FirstInfo & SecondInfo;
 
 const Page = () => {
   const [userInfo, setUserInfo] = useState<UserInformation>({
@@ -15,6 +16,11 @@ const Page = () => {
     name: '',
     studentId: '',
     phoneNumber: '',
+    majorId: -1,
+    minorId: -1,
+    doubleMajorId: -1,
+    career: -1,
+    careerDetail: '',
   });
   const [isPhaseOne, setIsPhaseOne] = useState<boolean>(true);
 
@@ -23,15 +29,20 @@ const Page = () => {
     setIsPhaseOne(false);
   };
 
-  useEffect(() => {
-    console.log(JSON.stringify(userInfo, null, 2));
-  }, [userInfo]);
+  const handleSubmitButtonClick = (value: SecondInfo) => {
+    setUserInfo((prev) => ({ ...prev, ...value }));
+
+    const tempUserInfo = { ...userInfo, ...value };
+    console.log(tempUserInfo);
+    // TODO: 회원가입 api 연결
+  };
 
   return (
-    <div className="w-sign mx-auto pb-10 pt-14 lg:pt-20">
-      <div className="flex flex-col gap-10 p-5 lg:pt-20">
+    <div className="w-sign mx-auto max-w-full pb-10 pt-14 lg:pt-20">
+      <div className="flex w-full flex-col gap-10 p-5">
         <PageTitle title="회원가입" description="PNU SW역량시스템 첫 사용시 회원가입이 필요합니다." urlText="" url="" />
         {isPhaseOne && <SignUpFirstPage handleSubmitButtonClick={handleNextButtonClick} />}
+        {!isPhaseOne && <SignUpSecondPage handleSubmitButtonClick={handleSubmitButtonClick} />}
       </div>
     </div>
   );
