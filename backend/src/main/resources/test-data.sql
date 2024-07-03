6d65bfe006766e2ca90e56ca7411997ea3a2fabc
amaranth <81465068+amaran-th@users.noreply.github.com>
2024-07-03 20:36:07 +0900

    feat: 기능 명세에 따른 API 명세 수정 (#79)
    - 마일스톤 Score 조회 시 Approve된 실적 내역만 취급하도록 수정
    - 마일스톤 실적 내역 조회 시 기간으로 필터링할 수 있도록 수정
    
    #78
    
    Co-authored-by: amaran-th <songsy405@naver.com>

--- backend/src/main/resources/test-data.sql
+++ backend/src/main/resources/test-data.sql
@@ -1,3 +1,4 @@
+## member(student)
 insert into member (email, name, password, phone_number, is_deleted)
 values ('songsy405@naver.com', '송세연', 'password', '010-0000-0000', false);
 
@@ -9,3 +10,32 @@ values ('songsy405@naver.com', '아마란스', 'password', '010-0000-0000', fals
 
 insert into student_member (id, member_id, major_id, minor_id, double_major_id, career, career_detail)
 values (202055555, 2, 1, null, null, '취업', 'IT 기업 개발자');
+
+## milestone histories
+INSERT INTO sw_css.milestone_history (id, milestone_id, student_id, description, file_url, status, reject_reason, count,
+                                      activated_at, is_deleted, created_at)
+VALUES (1, 2, 202055558, 'SW 관련 창업 - 교과', null, 'APPROVED', null, 1, '2024-06-01', 0, '2024-07-01 18:35:47.509352');
+INSERT INTO sw_css.milestone_history (id, milestone_id, student_id, description, file_url, status, reject_reason, count,
+                                      activated_at, is_deleted, created_at)
+VALUES (2, 3, 202055558, 'SW 관련 창업 - 비교과', null, 'APPROVED', null, 2, '2024-06-02', 0, '2024-07-01 18:35:47.559920');
+INSERT INTO sw_css.milestone_history (id, milestone_id, student_id, description, file_url, status, reject_reason, count,
+                                      activated_at, is_deleted, created_at)
+VALUES (3, 5, 202055558, 'TOPCIT 수준 2', null, 'APPROVED', null, 1, '2024-06-03', 0, '2024-07-01 18:35:47.563444');
+INSERT INTO sw_css.milestone_history (id, milestone_id, student_id, description, file_url, status, reject_reason, count,
+                                      activated_at, is_deleted, created_at)
+VALUES (4, 6, 202055558, 'PCC lv3', null, 'APPROVED', null, 1, '2024-06-04', 0, '2024-07-01 18:35:47.566673');
+INSERT INTO sw_css.milestone_history (id, milestone_id, student_id, description, file_url, status, reject_reason, count,
+                                      activated_at, is_deleted, created_at)
+VALUES (5, 7, 202055558, 'PCC lv2', null, 'APPROVED', null, 1, '2024-06-05', 0, '2024-07-01 18:35:47.569843');
+INSERT INTO sw_css.milestone_history (id, milestone_id, student_id, description, file_url, status, reject_reason, count,
+                                      activated_at, is_deleted, created_at)
+VALUES (6, 15, 202055558, '글로벌 행사 참여', null, 'APPROVED', null, 1, '2024-06-06', 0, '2024-07-01 18:35:47.571835');
+INSERT INTO sw_css.milestone_history (id, milestone_id, student_id, description, file_url, status, reject_reason, count,
+                                      activated_at, is_deleted, created_at)
+VALUES (7, 16, 202055558, '공인 영어 성적', null, 'APPROVED', null, 1, '2024-06-05', 0, '2024-07-01 18:35:47.574460');
+INSERT INTO sw_css.milestone_history (id, milestone_id, student_id, description, file_url, status, reject_reason, count,
+                                      activated_at, is_deleted, created_at)
+VALUES (8, 2, 202055558, 'SW 관련 창업 - 교과2', null, 'REJECTED', null, 1, '2024-06-04', 0, '2024-07-01 18:35:47.576742');
+INSERT INTO sw_css.milestone_history (id, milestone_id, student_id, description, file_url, status, reject_reason, count,
+                                      activated_at, is_deleted, created_at)
+VALUES (9, 15, 202055558, '글로벌 행사 참여', null, 'PENDING', null, 1, '2024-06-08', 0, '2024-07-01 18:35:47.580376');
