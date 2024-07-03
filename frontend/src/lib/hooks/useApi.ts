dccb456e58808a77edd7a67875674f7b0c2ba132
amaran-th <songsy405@naver.com>
2024-07-03 11:41:56 +0900

    feat: 에러 핸들링 코드 수정
    #25

--- frontend/src/lib/hooks/useApi.ts
+++ frontend/src/lib/hooks/useApi.ts
@@ -3,23 +3,15 @@ import { QueryKeys } from '@/data/queryKey';
 import { client } from '@/lib/api/client.axios';
 import { useAxiosQuery } from '@/lib/hooks/useAxios';
 import { CollegesResponseDto, MilestoneHistoryOfStudentResponseDto, MilestoneScoreDto } from '@/types/common.dto';
-import { BusinessError } from '@/types/error';
 
 export const useColleges = () =>
   useAxiosQuery({
     queryKey: QueryKeys.COLLEGES,
     queryFn: async (): Promise<CollegesResponseDto[] | null> => {
-      try {
-        const response = await client.get('/colleges');
-        return response.data;
-      } catch (error) {
-        if (error instanceof BusinessError) {
-          return null;
-        }
-        throw error;
-      }
+      const response = await client.get('/colleges');
+      return response?.data;
     },
-  });
+  }) ?? [];
 
 interface MilestoneScoreOfStudentProps {
   memberId: number;
@@ -31,17 +23,10 @@ export const useMilestoneScoresOfStudent = ({ memberId, startDate, endDate }: Mi
   useAxiosQuery({
     queryKey: QueryKeys.MILESTONE_SCORES_OF_STUDENT(memberId, startDate, endDate),
     queryFn: async (): Promise<MilestoneScoreDto[] | null> => {
-      try {
-        const response = await client.get(
-          `/milestones/histories/scores/members/${memberId}?start_date=${startDate}&end_date=${endDate}`,
-        );
-        return response.data;
-      } catch (error) {
-        if (error instanceof BusinessError) {
-          return null;
-        }
-        throw error;
-      }
+      const response = await client.get(
+        `/milestones/histories/scores/members/${memberId}?start_date=${startDate}&end_date=${endDate}`,
+      );
+      return response?.data;
     },
   });
 
@@ -55,14 +40,7 @@ export const useMilestoneHistoriesOfStudent = ({ memberId, startDate, endDate }:
   useAxiosQuery({
     queryKey: QueryKeys.MILESTONE_HISTORIES_OF_STUDENT(memberId, startDate, endDate),
     queryFn: async (): Promise<MilestoneHistoryOfStudentResponseDto[] | null> => {
-      try {
-        const response = await client.get(`/milestones/histories/members/${memberId}`);
-        return response.data;
-      } catch (error) {
-        if (error instanceof BusinessError) {
-          return null;
-        }
-        throw error;
-      }
+      const response = await client.get(`/milestones/histories/members/${memberId}`);
+      return response?.data;
     },
   });
