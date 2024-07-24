e183a11c90824e50c6e6848e86da55bda5961d29
amaran-th <songsy405@naver.com>
2024-07-24 17:37:58 +0900

    feat: MilestoneGroupLabel을 공통컴포넌트로 구현

---
+++ frontend/src/components/MilestoneGroupLabel/index.tsx
@@ -0,0 +1,34 @@
+import { MilestoneGroup } from '@/data/milestone';
+import { convertMilestoneGroup } from '@/lib/utils/utils';
+
+interface MilestoneGroupLabelProps {
+  group: string;
+}
+
+const MilestoneGroupLabel = ({ group }: MilestoneGroupLabelProps) => {
+  const labelText = convertMilestoneGroup(group);
+  switch (group) {
+    case MilestoneGroup.ACTIVITY:
+      return (
+        <span className="rounded-sm bg-milestone-blue-light px-2 py-1 text-xs text-milestone-blue-dark">
+          {labelText}
+        </span>
+      );
+    case MilestoneGroup.GLOBAL:
+      return (
+        <span className="rounded-sm bg-milestone-green-light px-2 py-1 text-xs text-milestone-green-dark">
+          {labelText}
+        </span>
+      );
+    case MilestoneGroup.COMMUNITY:
+      return (
+        <span className="rounded-sm bg-milestone-purple-light px-2 py-1 text-xs text-milestone-purple-dark">
+          {labelText}
+        </span>
+      );
+    default:
+      return <span className="rounded-sm bg-milestone-gray-light px-2 py-1 text-xs">{labelText}</span>;
+  }
+};
+
+export default MilestoneGroupLabel;
