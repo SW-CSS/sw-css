173e2c069d92f35749c7b4a01135493ea435d085
amaran-th <songsy405@naver.com>
2024-08-20 13:22:09 +0900

    feat: 모달 바깥 영역 클릭 시 모달을 닫는 커스텀 훅 구현
    #160

--- frontend/src/app/(withSidebar)/hackathon/[slug]/components/HackathonTeamReadModal/index.tsx
+++ frontend/src/app/(withSidebar)/hackathon/[slug]/components/HackathonTeamReadModal/index.tsx
@@ -1,23 +1,42 @@
 import useBodyScrollLock from '@/lib/hooks/useBodyScrollLock';
+import useOnClickOutside from '@/lib/hooks/useOnClickOutside';
 import { HackathonTeamDto } from '@/types/common.dto';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useCallback, useEffect, useRef } from 'react';
 
 interface HackathonTeamReadModalProps {
   selectedTeam: HackathonTeamDto | null;
   setSelectedTeam: Dispatch<SetStateAction<HackathonTeamDto | null>>;
 }
 const HackathonTeamReadModal = ({ selectedTeam, setSelectedTeam }: HackathonTeamReadModalProps) => {
+  const ref = useRef<HTMLDivElement>(null);
   const { lockScroll, unlockScroll } = useBodyScrollLock();
-  if (selectedTeam === null) {
-    unlockScroll();
+
+  const onClose = useCallback(() => {
+    setSelectedTeam(null);
+  }, [setSelectedTeam]);
+
+  useOnClickOutside(ref, onClose);
+
+  useEffect(() => {
+    if (selectedTeam) {
+      lockScroll();
+    } else {
+      unlockScroll();
+    }
+
+    return () => unlockScroll();
+  }, [selectedTeam, lockScroll, unlockScroll]);
+
+  if (!selectedTeam) {
     return null;
   }
-  lockScroll();
   return (
-    <div className="fixed inset-0 z-[51] flex h-[100vh] w-[100vw] items-center justify-center bg-black bg-opacity-30">
-      <div>
-        {selectedTeam.name}
-        <button onClick={() => setSelectedTeam(null)}>fjslfjsdl</button>
+    <div className="fixed inset-0 z-[51] flex items-center justify-center bg-black bg-opacity-30">
+      <div ref={ref} className="flex h-40 w-full max-w-80 flex-col items-center justify-center gap-4 rounded bg-white">
+        <p>{selectedTeam.name}</p>
+        <button onClick={onClose} className="rounded bg-comment px-4 py-2">
+          닫기
+        </button>
       </div>
     </div>
   );
