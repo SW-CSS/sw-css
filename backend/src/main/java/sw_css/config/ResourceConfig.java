d02e09f347c55eb7f7ce2d6bafcd057938bd6a0a
amaranth <81465068+amaran-th@users.noreply.github.com>
2024-08-22 16:20:27 +0900

    Feature/#155 url로 파일 리소스에 접근할 수 있도록 설정 (#157)
    * refactor: 파일 다운로드 기능 deprecated
    
    #155
    
    * feat: 외부에서 파일 리소스에 직접 접근할 수 있도록 설정
    
    #155
    
    * feat: 외부에서 파일 리소스에 직접 접근할 수 있도록 설정
    
    #155
    
    * fix: 파일 다운로드 테스트 삭제
    
    #155
    
    ---------
    
    Co-authored-by: amaran-th <songsy405@naver.com>

---
+++ backend/src/main/java/sw_css/config/ResourceConfig.java
@@ -0,0 +1,21 @@
+package sw_css.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig implements WebMvcConfigurer {
+
+    private String connectPath = "/files/**";
+
+    @Value("${data.file-storage-path}")
+    private String resourcePath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(connectPath)
+                .addResourceLocations("file:" + resourcePath);
+    }
+}
