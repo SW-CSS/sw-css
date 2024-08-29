c7e3e94f262aa1d4e0766ee4137b7190f6522857
llddang <bodyness@naver.com>
2024-08-29 16:41:52 +0900

    feat: super admin 어노테이션 구현
    #170

---
+++ backend/src/main/java/sw_css/utils/annotation/SuperAdmin.java
@@ -0,0 +1,12 @@
+package sw_css.utils.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SuperAdmin {
+    boolean required() default true;
+}
