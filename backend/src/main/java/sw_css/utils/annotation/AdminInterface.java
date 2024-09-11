22196029b53ed9d5df1edd5e3e9a8cff38a63246
llddang <bodyness@naver.com>
2024-09-11 17:49:01 +0900

    refactor: jwt annotation 명 수정
    #186

--- backend/src/main/java/sw_css/utils/annotation/AdminInterface.java
+++ backend/src/main/java/sw_css/utils/annotation/AdminInterface.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Admin {
+public @interface AdminInterface {
     boolean required() default true;
 }
