0ae48f20cce4dc886b2b9d39ae06823ca225f851
llddang <bodyness@naver.com>
2024-09-13 02:38:53 +0900

    design: z-index 수정 및 uri 가 달라졌을 대 sidebar 닫히도록 수정
    #195

---
+++ frontend/src/app/(client)/components/Sidebar/index.tsx
@@ -0,0 +1,82 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+'use client';
+
+import { VscChevronDown } from '@react-icons/all-files/vsc/VscChevronDown';
+import { VscChevronUp } from '@react-icons/all-files/vsc/VscChevronUp';
+import { usePathname } from 'next/navigation';
+import { useCallback, useEffect, useState } from 'react';
+
+import { headerInfos } from '@/data/clientCategory';
+import { CategoryDto, SubCategoryDto } from '@/types/common.dto';
+
+import * as S from './styled';
+
+const Sidebar = () => {
+  const pathname = usePathname();
+  const [currentCategory, setCurrentCategory] = useState<CategoryDto | null>();
+  const [currentSubCategory, setCurrentSubCategory] = useState<SubCategoryDto | null>();
+  const [isOpenNavigationBar, setIsOpenNavigationBar] = useState<boolean>(false);
+
+  const findMatchPath = useCallback(() => {
+    let maxOverlap = 0;
+    let bestMatch: SubCategoryDto | null = null;
+    currentCategory?.sub.forEach((category) => {
+      const { url } = category;
+      if (pathname.startsWith(url) && url.length > maxOverlap) {
+        maxOverlap = url.length;
+        bestMatch = category;
+      }
+    });
+    return bestMatch;
+  }, [pathname, currentCategory]);
+
+  useEffect(() => {
+    setCurrentCategory(headerInfos.filter((headerInfo) => pathname.startsWith(headerInfo.url))[0]);
+    setIsOpenNavigationBar(false);
+  }, [pathname]);
+
+  useEffect(() => {
+    setCurrentSubCategory(findMatchPath());
+  }, [findMatchPath]);
+
+  return (
+    <>
+      <S.SidebarWrapper>
+        <S.SidebarCategoryTitle>{currentCategory?.title}</S.SidebarCategoryTitle>
+        <S.SidebarCategoryDescription>{currentCategory?.description}</S.SidebarCategoryDescription>
+        <S.SidebarCategoryList isOpen={isOpenNavigationBar}>
+          {currentCategory?.sub.map((sub) => (
+            <S.SidebarCategory
+              isCurrentCategory={sub.title === currentSubCategory?.title}
+              key={sub.title}
+              href={sub.url}
+            >
+              {sub.title}
+            </S.SidebarCategory>
+          ))}
+        </S.SidebarCategoryList>
+      </S.SidebarWrapper>
+      <S.SidebarMobileWrapper>
+        <S.SidebarMobileButton onClick={() => setIsOpenNavigationBar((prev) => !prev)}>
+          {currentSubCategory?.title}
+          {isOpenNavigationBar ? <VscChevronUp color="white" /> : <VscChevronDown color="white" />}
+        </S.SidebarMobileButton>
+
+        <S.SidebarCategoryList isOpen={isOpenNavigationBar}>
+          {currentCategory?.sub.map((sub) => (
+            <S.SidebarCategory
+              isCurrentCategory={sub.title === currentSubCategory?.title}
+              key={sub.title}
+              href={sub.url}
+            >
+              {sub.title}
+            </S.SidebarCategory>
+          ))}
+        </S.SidebarCategoryList>
+      </S.SidebarMobileWrapper>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ frontend/src/app/(client)/components/Sidebar/styled.ts
@@ -0,0 +1,120 @@
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable operator-linebreak */
+
+'use client';
+
+import Link from 'next/link';
+import styled from 'styled-components';
+
+import { COLOR, FONT_STYLE, RESPONSIVE_WIDTH } from '@/constants';
+
+interface SidebarCategoryProps {
+  isCurrentCategory: boolean;
+}
+
+interface SidebarCategoryListProps {
+  isOpen: boolean;
+}
+
+export const SidebarWrapper = styled.div`
+  width: 290px;
+  min-height: calc(100vh - 200px);
+  padding: 120px 20px 0px 20px;
+  border-right: 1px solid ${COLOR.border};
+  background-color: ${COLOR.white};
+  @media screen and (max-width: ${RESPONSIVE_WIDTH.desktop}) {
+    display: none;
+  }
+`;
+
+export const SidebarCategoryTitle = styled.p`
+  font: ${FONT_STYLE.xl.semibold};
+`;
+
+export const SidebarCategoryDescription = styled.p`
+  font: ${FONT_STYLE.sm.normal};
+  color: ${COLOR.comment};
+  margin-top: 30px;
+  word-break: keep-all;
+`;
+
+export const SidebarCategoryList = styled.div<SidebarCategoryListProps>`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  margin-top: 30px;
+  flex-direction: column;
+  z-index: 49;
+
+  @media screen and (max-width: ${RESPONSIVE_WIDTH.desktop}) {
+    transition: all 0.6s ease-in-out;
+    position: absolute;
+    top: 45px;
+    left: 0;
+    height: fit-content;
+    max-height: 0;
+    overflow: hidden;
+    margin-top: 0px;
+    background-color: ${COLOR.background.light};
+    ${({ isOpen }) => isOpen && 'max-height: 100vh;'}
+  }
+`;
+
+export const SidebarCategory = styled(Link)<SidebarCategoryProps>`
+  font: ${FONT_STYLE.lg.semibold};
+  margin-bottom: 20px;
+  color: ${COLOR.comment};
+  line-height: 40px;
+
+  ${({ isCurrentCategory }) =>
+    isCurrentCategory &&
+    `color: ${COLOR.black_text};
+    text-decoration: underline;
+    text-underline-offset: 12px;
+  `}
+
+  @media screen and (max-width: ${RESPONSIVE_WIDTH.desktop}) {
+    margin-bottom: 0px;
+    padding: 10px 15px;
+    border-bottom: 1px solid ${COLOR.border};
+    color: ${COLOR.comment};
+    font: ${FONT_STYLE.sm.normal};
+    ${({ isCurrentCategory }) =>
+      isCurrentCategory &&
+      `color: ${COLOR.primary.main};
+        text-decoration: none;`};
+  }
+`;
+
+export const SidebarMobileWrapper = styled.div`
+  width: 100%;
+  margin-top: 50px;
+  height: inherit;
+  position: relative;
+
+  @media screen and (min-width: ${RESPONSIVE_WIDTH.desktop}) {
+    display: none;
+  }
+`;
+
+export const SidebarMobileButton = styled.button`
+  display: none;
+  width: 100%;
+  height: 45px;
+  background-color: ${COLOR.secondary.main};
+  color: ${COLOR.white};
+  padding: 0px 15px;
+  z-index: 49;
+
+  @media screen and (max-width: ${RESPONSIVE_WIDTH.desktop}) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &:active,
+    &:focus {
+      outline: none;
+      border: none;
+    }
+  }
+`;
