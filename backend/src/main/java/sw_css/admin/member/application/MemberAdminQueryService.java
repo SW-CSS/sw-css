a70880aa1537acffd97f3038adabd13fad91dd00
llddang <bodyness@naver.com>
2024-09-21 23:42:57 +0900

    feat: 교직원 검색 수정
    #210

--- backend/src/main/java/sw_css/admin/member/application/MemberAdminQueryService.java
+++ backend/src/main/java/sw_css/admin/member/application/MemberAdminQueryService.java
@@ -10,6 +10,7 @@ import sw_css.admin.member.application.dto.response.FacultyMemberResponse;
 import sw_css.member.application.dto.response.StudentMemberResponse;
 import sw_css.member.domain.FacultyMember;
 import sw_css.member.domain.StudentMember;
+import sw_css.member.domain.repository.FacultyMemberCustomRepository;
 import sw_css.member.domain.repository.FacultyMemberRepository;
 import sw_css.member.domain.repository.StudentMemberRepository;
 
@@ -18,7 +19,7 @@ import sw_css.member.domain.repository.StudentMemberRepository;
 @Transactional(readOnly = true)
 public class MemberAdminQueryService {
     private final StudentMemberRepository studentMemberRepository;
-    private final FacultyMemberRepository facultyMemberRepository;
+    private final FacultyMemberCustomRepository facultyMemberCustomRepository;
 
     public List<StudentMemberResponse> findStudentMembers() {
         final List<StudentMember> students = studentMemberRepository.findAll();
@@ -26,7 +27,7 @@ public class MemberAdminQueryService {
     }
 
     public Page<FacultyMemberResponse> findFacultyMembers(final Integer field, final String keyword, final Pageable pageable) {
-        final Page<FacultyMember> faculties = facultyMemberRepository.findFacultyMembersBy(field, keyword, pageable);
+        final Page<FacultyMember> faculties = facultyMemberCustomRepository.findFacultyMembers(field, keyword, pageable);
 
         return FacultyMemberResponse.from(faculties, pageable);
     }
--- backend/src/main/java/sw_css/member/domain/repository/FacultyMemberRepository.java
+++ backend/src/main/java/sw_css/member/domain/repository/FacultyMemberRepository.java
@@ -1,18 +1,9 @@
 package sw_css.member.domain.repository;
 
 import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.Nullable;
 import sw_css.member.domain.FacultyMember;
 
 public interface FacultyMemberRepository extends JpaRepository<FacultyMember, Long> {
-    boolean existsByMemberId(Long memberId);
-
     Optional<FacultyMember> findByMemberId(Long memberId);
-
-    Page<FacultyMember> findFacultyMembersBy(@Nullable final Integer field,
-                                             @Nullable final String keyword,
-                                             final Pageable pageable);
 }
