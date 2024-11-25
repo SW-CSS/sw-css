e28ebe3c393a4b8a161c45b11852f7ebcb806ef6
llddang <bodyness@naver.com>
2024-11-25 16:14:37 +0900

    design: 사이드바가 있는 layout 수정
    #227

--- frontend/src/components/layout/SidebarLayout.tsx
+++ frontend/src/components/layout/SidebarLayout.tsx
@@ -5,7 +5,7 @@ export default function SidebarLayout({ children }: Readonly<{ children: React.R
     <div className="block w-full bg-white lg:flex lg:bg-background-light" style={{ minHeight: 'calc(100vh-200px)' }}>
       <Sidebar />
       <div className="grow">
-        <div className="lg:w-client-content w-full overflow-hidden p-5 lg:pt-[100px]">{children}</div>
+        <div className="lg:w-client-content mt-[50px] w-full overflow-hidden p-5 lg:mt-[76px]">{children}</div>
       </div>
     </div>
   );
