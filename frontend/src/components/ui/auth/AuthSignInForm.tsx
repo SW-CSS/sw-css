96293971e29681b0ae3a3ee8de2a2ad81d579ca9
llddang <bodyness@naver.com>
2024-11-28 16:16:33 +0900

    refactor: 로그인 폼 css 라이브러리 변경
    #227

--- frontend/src/components/ui/auth/AuthSignInForm.tsx
+++ frontend/src/components/ui/auth/AuthSignInForm.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-
-import { InputFixedText, Input, InputLabel, InputWrapper, SignButton } from '@/app/(client)/(auth)/styled';
-import { useAppDispatch, useAppSelector } from '@/lib/hooks/redux';
-import { signIn } from '@/store/auth.slice';
-import { useSignInMutation } from '@/lib/hooks/useApi';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { toast } from 'react-toastify';
+
+import { useAppDispatch, useAppSelector } from '@/lib/hooks/redux';
+import { useSignInMutation } from '@/lib/hooks/useApi';
+import { signIn } from '@/store/auth.slice';
 
 export default function AuthSignInForm() {
   const [userInfo, setUserInfo] = useState({
@@ -55,25 +54,38 @@ export default function AuthSignInForm() {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <InputWrapper>
-          <InputLabel htmlFor="email">아이디(이메일)</InputLabel>
-          <Input id="email" placeholder="아이디 입력" value={userInfo.email} onChange={handleInputChange} />
-          <InputFixedText>@pusan.ac.kr</InputFixedText>
-        </InputWrapper>
-        <InputWrapper>
-          <InputLabel htmlFor="password">비밀번호</InputLabel>
-          <Input
-            type="password"
-            id="password"
-            placeholder="비밀번호 입력"
-            value={userInfo.password}
-            onChange={handleInputChange}
-          />
-        </InputWrapper>
-        <SignButton type="submit">로그인</SignButton>
-      </form>
-    </>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+      <div className="relative flex flex-col">
+        <label className="text-sm font-semibold" htmlFor="email">
+          아이디(이메일)
+        </label>
+        <input
+          className="rounded-sm border border-border p-2 outline-none placeholder:italic placeholder:text-border"
+          id="email"
+          placeholder="아이디 입력"
+          value={userInfo.email}
+          onChange={handleInputChange}
+        />
+        <div className="absolute right-2.5 translate-y-1/2 text-sm text-comment" style={{ bottom: 'calc(50% - 10px)' }}>
+          @pusan.ac.kr
+        </div>
+      </div>
+      <div className="relative flex flex-col">
+        <label className="text-sm font-semibold" htmlFor="password">
+          비밀번호
+        </label>
+        <input
+          className="rounded-sm border border-border p-2 outline-none placeholder:italic placeholder:text-border"
+          type="password"
+          id="password"
+          placeholder="비밀번호 입력"
+          value={userInfo.password}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button className="mt-4 w-full rounded-sm border-0 bg-primary-main p-2 text-white" type="submit">
+        로그인
+      </button>
+    </form>
   );
 }
