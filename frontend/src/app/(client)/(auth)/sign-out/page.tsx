ace7436f8ccd5503af8db614b09259aea5285fe7
llddang <bodyness@naver.com>
2024-11-28 16:59:53 +0900

    refactor: 로그아웃 페이지
    #227

--- frontend/src/app/(client)/(auth)/sign-out/page.tsx
+++ frontend/src/app/(client)/(auth)/sign-out/page.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-
 'use client';
 
 import { useRouter } from 'next/navigation';
@@ -7,20 +5,17 @@ import { useRouter } from 'next/navigation';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks/redux';
 import { signOut } from '@/store/auth.slice';
 
-const Page = () => {
+export default function SignOutPage() {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const auth = useAppSelector((state) => state.auth).value;
 
   if (!auth.isAuth) router.push('/');
 
-  // TODO: api 연결
   dispatch(signOut());
   setTimeout(() => {
     router.refresh();
   }, 0);
 
-  return null;
-};
-
-export default Page;
+  return <></>;
+}
