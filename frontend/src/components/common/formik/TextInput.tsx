c595200b64d95643e3830ddd5f1f1705ad696531
llddang <bodyness@naver.com>
2024-11-28 15:29:32 +0900

    feat: textInput에 툴팁 기능 추가
    #227

--- frontend/src/components/common/formik/TextInput.tsx
+++ frontend/src/components/common/formik/TextInput.tsx
@@ -1,6 +1,7 @@
-/* eslint-disable @typescript-eslint/indent */
-/* eslint-disable max-len */
+'use client';
+
 import { FORM_SIZE } from '@/constants';
+import { VscInfo } from '@react-icons/all-files/vsc/VscInfo';
 
 type BuiltInTextInputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
@@ -11,6 +12,7 @@ interface CustomTextInputProps {
   errorText?: string;
   size?: 'sm' | 'md' | 'lg';
   isAdmin?: boolean;
+  tooltip?: string;
   onKeyDownEnter?(): void;
   onChangeText?(text: string): void;
 }
@@ -18,13 +20,21 @@ interface CustomTextInputProps {
 export type TextInputProps = Omit<BuiltInTextInputProps, 'size'> & CustomTextInputProps;
 
 const TextInput = ({ isRequired = false, size = 'md', ...props }: TextInputProps) => {
-  const { label, errorText, onKeyDownEnter, onChangeText, isAdmin, ...inputProps } = props;
+  const { value, label, errorText, isAdmin, tooltip, onKeyDownEnter, onChangeText, ...inputProps } = props;
   const hasError = errorText !== undefined;
 
   return (
     <div className="flex flex-grow flex-col gap-1">
       {label && (
-        <label htmlFor={inputProps.name} className={`${FORM_SIZE[size].subTextSize} font-semibold`}>
+        <label htmlFor={inputProps.name} className={`flex items-center ${FORM_SIZE[size].subTextSize} font-semibold`}>
+          {tooltip && (
+            <div className="relative flex items-center gap-1 px-2 py-1 text-xs">
+              <VscInfo className="peer h-[14px] w-[14px]" />
+              <div className="absolute left-0 top-1 hidden -translate-y-[calc(100%+4px)] whitespace-nowrap break-keep rounded border bg-white p-2 peer-hover:block">
+                {tooltip}
+              </div>
+            </div>
+          )}
           {label} {isRequired && <span className={`${FORM_SIZE[size].subTextSize} font-semibold text-red-400`}>*</span>}
         </label>
       )}
