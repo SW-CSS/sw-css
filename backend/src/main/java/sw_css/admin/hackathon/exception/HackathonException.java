c6fe4f7e09e48bd9184b9dbb683481b5d908974e
llddang <bodyness@naver.com>
2024-12-19 21:09:09 +0900

    feat: 해커톤 관련 예외 및 예외 타입 정의
    #165

---
+++ backend/src/main/java/sw_css/admin/hackathon/exception/HackathonException.java
@@ -0,0 +1,16 @@
+package sw_css.admin.hackathon.exception;
+
+import sw_css.base.BaseException;
+import sw_css.base.BaseExceptionType;
+
+public class HackathonException extends BaseException {
+    private final HackathonExceptionType exceptionType;
+
+    public HackathonException(final HackathonExceptionType exceptionType) {
+        super((exceptionType.errorMessage()));
+        this.exceptionType = exceptionType;
+    }
+
+    @Override
+    public BaseExceptionType exceptionType() { return exceptionType; }
+}
---
+++ backend/src/main/java/sw_css/admin/hackathon/exception/HackathonExceptionType.java
@@ -0,0 +1,29 @@
+package sw_css.admin.hackathon.exception;
+
+import org.springframework.http.HttpStatus;
+import sw_css.base.BaseExceptionType;
+
+public enum HackathonExceptionType implements BaseExceptionType {
+    NOT_EXIST_FILE(HttpStatus.BAD_REQUEST, "파일을 첨부해야 합니다."),
+    INVALID_APPLY_DATE(HttpStatus.BAD_REQUEST, "신청 시작일이 신청 마지막날 보다 이후일 수 없습니다."),
+    INVALID_HACKATHON_DATE(HttpStatus.BAD_REQUEST, "대회 시작일이 대회 마지막날 보다 이후일 수 없습니다."),
+    UNSUPPORTED_FILE_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "파일 유형이 png, jpg, jpeg 가 아닙니다.");
+
+    private final HttpStatus httpStatus;
+    private final String errorMessage;
+
+    HackathonExceptionType(final HttpStatus httpStatus, final String errorMessage) {
+        this.httpStatus = httpStatus;
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public HttpStatus httpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String errorMessage() {
+        return errorMessage;
+    }
+}
